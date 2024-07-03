015ed677b192720923945a42a45e2267b6947533
Shreyas Patange <115157819+IWhitebird@users.noreply.github.com>
2024-07-03 12:45:20 +0530

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true })
+  );
+
+  const config = new DocumentBuilder()
+  .setTitle('API DOCS')
+  .addTag('Identity-Reconciliation')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+  Logger.log(`Api docs running on http://localhost:3000/api`, 'Bootstrap')
+}
+bootstrap();
